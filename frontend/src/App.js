fae53b6ee520a15d5ad708a8d620fd2945b84c2f
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-10-24 16:42:45 +0800

    feat: 用react-router把路線分配給頁面

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './views/Home.js'
+import FAQs from './views/FAQs.js'
+import Terms from './views/Terms.js'
+import Store from './views/Store.js'
+import StoreItemPage from './views/StoreItemPage.js'
+import ShoppingCart from './views/ShoppingCart.js'
+import ShoppingPay from './views/ShoppingPay.js'
+import UserLogin from './views/UserLogin.js'
+import UserRegister from './views/UserRegister.js'
+import UserHistory from './views/UserHistory.js'
+import UserSettings from './views/UserSettings.js'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/faqs",
+    element: <FAQs />,
+  },
+  {
+    path: "/terms",
+    element: <Terms />,
+  },
+  {
+    path: "/store",
+    element: <Store />,
+  },
+  {
+    path: "/store/:id",
+    element: <StoreItemPage />,
+  },
+  {
+    path: "/shopping/cart",
+    element: <ShoppingCart />,
+  },
+  {
+    path: "/shopping/pay",
+    element: <ShoppingPay />,
+  },
+  {
+    path: "/user/login",
+    element: <UserLogin />,
+  },
+  {
+    path: "/user/register",
+    element: <UserRegister />,
+  },
+  {
+    path: "/user/history",
+    element: <UserHistory />,
+  },
+  {
+    path: "/user/settings",
+    element: <UserSettings />,
+  },
+]);
 
 function App() {
   return (
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
