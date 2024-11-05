67263f938612b786fa56e555ac952938336a778d
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-11-05 15:08:39 +0800

    feat：製造首頁基本結構

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,5 +1,13 @@
 export default function Home() {
     return (
-        <main></main>
+        <main className='home-main'>
+            <img src='./assets/jjs.jpg' className='home-hero-banner'></img>
+            <div className='home-newest'>
+                <h2 className='home-newest-title'>最新最紅的產品</h2>
+                <div className='home-newest-list'>
+
+                </div>
+            </div>
+        </main>
     )
 }
