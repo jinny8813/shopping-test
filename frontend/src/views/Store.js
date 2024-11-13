c92e34e6a6d177eeca2eea780e8d6bd385e9d335
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-11-13 15:54:51 +0800

    feat：完成線上商店頁面

--- frontend/src/views/Store.js
+++ frontend/src/views/Store.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
 import StoreFilters from '../components/StoreFilters'
+import StoreItem from '../components/StoreItem'
 
 export default function Store() {
     return (
         <React.Fragment>
             <StoreFilters />
             <main className='store-main'>
+                <StoreItem></StoreItem>
+                <StoreItem></StoreItem>
+                <StoreItem></StoreItem>
+                <StoreItem></StoreItem>
+                <StoreItem></StoreItem>
             </main>
         </React.Fragment>
     )
