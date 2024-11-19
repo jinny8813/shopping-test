07dfa79415c9a83c1e0aedbf8eccb3c7ee96ce04
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-11-19 16:32:10 +0800

    feat：完成使用者註冊頁面

--- frontend/src/views/UserRegister.js
+++ frontend/src/views/UserRegister.js
@@ -7,13 +7,17 @@ export default function UserRegister() {
             <form className="user-register-form">
                 <h2 className="user-register-form-title">使用者註冊</h2>
                 <div className="user-register-form-inputs">
-                    <Input size="medium" addonBefore={<UserOutlined />} className="user-register-form-input" placeholder="名字"></Input>
-                    <Input size="medium" addonBefore={<KeyOutlined />} className="user-register-form-input" type="password" placeholder="密碼"></Input>
-                    <Input size="medium" addonBefore={<MailOutlined />} className="user-register-form-input" type="email" placeholder="電子郵件"></Input>
+                    <Input size="large" addonBefore={<UserOutlined />} className="user-register-form-input" placeholder="名字"></Input>
+                    <Input size="large" addonBefore={<KeyOutlined />} className="user-register-form-input" type="password" placeholder="密碼"></Input>
+                    <Input size="large" addonBefore={<MailOutlined />} className="user-register-form-input" type="email" placeholder="電子郵件"></Input>
                 </div>
                 <div className="user-register-form-thirdp">
-                    <button className="user-register-form-thirdp-btn user-register-form-thirdp-line"><img className="user-register-form-thirdp-line-logo" src="/assets/line.png"></img><span className="user-register-form-thirdp-line-text">與LINE帳號登入</span></button>
-                    <button className="user-register-form-thirdp-btn user-register-form-thirdp-google">Google</button>
+                    <button className="user-register-form-thirdp-btn user-register-form-thirdp-line">
+                        <img className="user-register-form-thirdp-line-logo" src="/assets/line.png"></img>
+                        <span className="user-register-form-thirdp-line-text">與LINE帳號登入</span></button>
+                    <button className="user-register-form-thirdp-btn user-register-form-thirdp-google">
+                        <img className="user-register-form-thirdp-google-logo" src="/assets/google.png"></img>
+                        <span className="user-register-form-thirdp-google-text">使用Google帳號登入</span></button>
                 </div>
                 <button className="user-register-form-btn"><SendOutlined /> 註冊帳號</button>
             </form>
