5e7859ec40fa2f0f91ed8e44fcb3fb7ad9b37d66
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-11-19 16:36:10 +0800

    feat：製造使用者登入頁面

--- frontend/src/views/UserLogin.js
+++ frontend/src/views/UserLogin.js
@@ -1,5 +1,25 @@
+import { Input } from 'antd'
+import { UserOutlined, KeyOutlined, MailOutlined, SendOutlined } from '@ant-design/icons'
+
 export default function UserLogin() {
     return (
-        <main></main>
+        <main className="user-register-main">
+            <form className="user-register-form">
+                <h2 className="user-register-form-title">使用者登入</h2>
+                <div className="user-register-form-inputs">
+                    <Input size="large" addonBefore={<UserOutlined />} className="user-register-form-input" placeholder="名字"></Input>
+                    <Input size="large" addonBefore={<KeyOutlined />} className="user-register-form-input" type="password" placeholder="密碼"></Input>
+                </div>
+                <div className="user-register-form-thirdp">
+                    <button className="user-register-form-thirdp-btn user-register-form-thirdp-line">
+                        <img className="user-register-form-thirdp-line-logo" src="/assets/line.png"></img>
+                        <span className="user-register-form-thirdp-line-text">與LINE帳號登入</span></button>
+                    <button className="user-register-form-thirdp-btn user-register-form-thirdp-google">
+                        <img className="user-register-form-thirdp-google-logo" src="/assets/google.png"></img>
+                        <span className="user-register-form-thirdp-google-text">使用Google帳號登入</span></button>
+                </div>
+                <button className="user-register-form-btn"><SendOutlined /> 帳號登入</button>
+            </form>
+        </main>
     )
 }
