85cd5c6a25c001f7d46a8f16e404bcf16ec02bc7
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-11-20 15:19:40 +0800

    feat：完成使用者設定css

--- frontend/src/views/UserSettings.js
+++ frontend/src/views/UserSettings.js
@@ -3,37 +3,36 @@ import { EditFilled } from '@ant-design/icons'
 export default function UserSettings() {
     return (
         <main className='user-settings'>
-            <h2 className='user-settings-title'>使用者資料修改</h2>
-            <hr className='user-settings-break'></hr>
+            <div className='user-settings-top'>
+                <h2 className='user-settings-title'>使用者資料修改</h2>
+            </div>
             <form className='user-settings-form'>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td className='user-settings-form-field'>姓名</td>
-                            <td className='user-settings-form-data'>柯文哲</td>
-                            <td className='user-settings-form-edit-btn'><button className='user-settings'><EditFilled /> 修改</button></td>
-                        </tr>
-                        <tr>
-                            <td className='user-settings-form-field'>密碼</td>
-                            <td className='user-settings-form-data'>**********</td>
-                            <td className='user-settings-form-edit-btn'><button className='user-settings'><EditFilled /> 修改</button></td>
-                        </tr>
-                        <tr>
-                            <td className='user-settings-form-field'>電子郵件</td>
-                            <td className='user-settings-form-data'>kp@gmail.com</td>
-                            <td className='user-settings-form-edit-btn'><button className='user-settings'><EditFilled /> 修改</button></td>
-                        </tr>
-                        <tr>
-                            <td className='user-settings-form-field'>LINE帳號</td>
-                            <td className='user-settings-form-data'>kewenzhe</td>
-                            <td className='user-settings-form-edit-btn'><button className='user-settings'><EditFilled /> 修改</button></td>
-                        </tr>
-                        <tr>
-                            <td className='user-settings-form-field'>電話號碼</td>
-                            <td className='user-settings-form-data'>0979123456</td>
-                            <td className='user-settings-form-edit-btn'><button className='user-settings'><EditFilled /> 修改</button></td>
-                        </tr>
-                    </tbody>
+                <table className='user-settings-table'>
+                    <tr>
+                        <td className='user-settings-table-field'>姓名</td>
+                        <td className='user-settings-table-data'>柯文哲</td>
+                        <td className='user-settings-table-edit-btn'><button type='submit'><EditFilled /> 修改</button></td>
+                    </tr>
+                    <tr>
+                        <td className='user-settings-table-field'>密碼</td>
+                        <td className='user-settings-table-data'>k**********p</td>
+                        <td className='user-settings-table-edit-btn'><button type='submit'><EditFilled /> 修改</button></td>
+                    </tr>
+                    <tr>
+                        <td className='user-settings-table-field'>電子郵件</td>
+                        <td className='user-settings-table-data'>kp@gmail.com</td>
+                        <td className='user-settings-table-edit-btn'><button type='submit'><EditFilled /> 修改</button></td>
+                    </tr>
+                    <tr>
+                        <td className='user-settings-table-field'>LINE帳號</td>
+                        <td className='user-settings-table-data'>kewenzhe</td>
+                        <td className='user-settings-table-edit-btn'><button type='submit'><EditFilled /> 修改</button></td>
+                    </tr>
+                    <tr>
+                        <td className='user-settings-table-field'>電話號碼</td>
+                        <td className='user-settings-table-data'>0979123456</td>
+                        <td className='user-settings-table-edit-btn'><button type='submit'><EditFilled /> 修改</button></td>
+                    </tr>
                 </table>
             </form>
         </main>
