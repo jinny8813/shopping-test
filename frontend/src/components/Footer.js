fdbf72aff90e894c73fce8f03b3b09bfd758b4a2
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2024-12-02 14:37:37 +0800

    feat: 製造頁腳

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer-text">騏騏資訊 © 2024 - 保留所有權利</p>
+        </footer>
+    )
+}
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,12 +2,14 @@ import React from "react"
 import { Outlet } from "react-router-dom"
 
 import Header from "./Header"
+import Footer from "./Footer"
 
 export default function Layout() {
     return (
         <React.StrictMode>
             <Header />
             <Outlet />
+            <Footer />
         </React.StrictMode>
     )
 }
